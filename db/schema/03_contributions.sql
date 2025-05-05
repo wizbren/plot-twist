51c8f47f6abdf117feda85fe5423104ee3b41ec6
Brendan Brown <brendan94brown@gmail.com>
2025-05-05 12:23:44 -0700

    Add third schema table for 03_contributions.sql

---
+++ db/schema/03_contributions.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS contributions CASCADE;
+
+
+CREATE TABLE contributions (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
+  text TEXT NOT NULL
+);
